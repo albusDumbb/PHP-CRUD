400c7eb8d72d678b971d8fc4a361b84e685787c9
hehehe <162644453+albusDumbb@users.noreply.github.com>
2025-12-03 08:51:24 +0800

    Update post.php

--- post.php
+++ post.php
@@ -1,67 +1,58 @@
 <?php
 
-// Load database connection from config.php file
 require_once "config.php";
 
 header("Content-Type: application/json");
 
-// Function for response
 function response($status, $message, $data = []) {
-    $arry = [
+    $array = [
         "status" => $status,
-        "message" => $message,
+        "message" => $message, 
         "data" => $data
     ];
 
-    $json = json_encode($arry);
+    $json = json_encode($array);
 
     echo $json;
 
     exit;
 }
 
-// Get and sanitize data from user
 $name = trim($_POST["name"] ?? "");
 $age = trim($_POST["age"] ?? "");
 $gender = trim($_POST["gender"] ?? "");
 
-// Empty array to store error messages
-$errors = [];
+$error = [];
 
-// Validdation of the data
 if ($name === "") {
-    $error[] = "name is required.";
+    $error[] = "name is required";
 } elseif (!preg_match("/^[a-zA-Z\s]+$/", $name)) {
-    $error[] = "name must be alphabet letters only";
+    $error[] = "name should be alphabet";
 }
 if ($age === "") {
-    $error[] = "age is required.";
-} elseif (!filter_var($age, FILTER_VALIDATE_INT)){
-    $error[] = "age must be numbers";
+    $error[] = "age is required";
+
+} elseif (!filter_var($age, FILTER_VALIDATE_INT)) {
+    $error[] = "age must be number";
 }
 if ($gender === "") {
-    $error[] = "gender is required.";
-} 
-
-/* If the $errors is not empty, then throw an error response through
-jsonResponse function */
-if (!empty($errors)) {
-    response(false, "Validation is invalid", $errors);
+    $error[] = "gender is required";
 }
 
-// Insert data to the database using prepared statement
-$stmt = $conn->prepare("INSERT INTO users (name, age, gender) VALUES (?, ?, ?)");
-$stmt->bind_param("sis", $name, $age, $gender); // sis means strings, integer, and strings
-
-// Run SQL query
-if ($stmt->execute()) {
-    response(true, "Data added successfuly!", [
-        "name" => $name,
-        "age" => $age,
-        "gender" => $gender
-    ]);
+if (!empty($error)) {
+    response(false, "validation failed", $error);
 } else {
-    response(false, "Data failed to add.", $stmt->error);
-}
+    $prepare = $connection->prepare("INSERT INTO users (name, age, gender) VALUES (?, ?, ?)");
+    $prepare->bind_param("sis", $name, $age, $gender);
 
+    if ($prepare->execute()) {
+        response(true, "data added successfuly", [
+            "name" => $name,
+            "age" => $age,
+            "gender" => $gender
+        ]);
+    } else {
+        response(false, "data added unsuccessfuly", $prepare->error);
+    }
+}
 ?>
